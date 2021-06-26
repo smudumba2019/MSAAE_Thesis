63276876bdbdab5e6d9720d6f09420648c639a26
Sai Mudumba <56524843+smudumba2019@users.noreply.github.com>
2021-06-26 15:31:44 -0500

    Add files via upload

---
+++ Code_v2/Aircraft.py
@@ -0,0 +1,239 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 15:33:44 2021
+
+@author: Sai Mudumba
+"""
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+class Aircraft:
+    def __init__(self, ACname, PAX, Speed_Cr, Range, LD_Ratio, Max_Flt_Time, MTOW, KWh_Batt, S):
+        # Later on, this will be replaced by loading an excel sheet that automatically loads all the characteristics
+        # This is written here only for ease of running the code
+        self.acName = ACname
+        self.pax = PAX
+        self.cruiseSpeed = Speed_Cr
+        self.acRange = Range
+        self.LDratio = LD_Ratio
+        self.maxFltTime = Max_Flt_Time
+        self.MTOW = MTOW
+        self.kWhBatt = KWh_Batt
+        self.S = S
+        self.DGlideStraight = 0
+      
+    def Characteristics(self):
+        print(f"These are the aircraft characteristics for {self.acName}: ")
+        print(f"Aircraft Name: {self.acName}")
+        print(f"Passenger Capacity: {self.pax}")
+        print(f"Maximum Cruise Speed (mph): {self.cruiseSpeed}")
+        print(f"Range (mi): {self.acRange}")
+        print(f"Lift to Drag Ratio (Cruise): {self.LDratio}")
+        print(f"Endurance (minutes): {self.maxFltTime}")
+        print(f"Maximum Takeoff Weight (kg): {self.MTOW}")
+        print(f"Battery Capacity (kWh): {self.kWhBatt}")
+        print(f"Wing Surface Area (m^3): {self.S}")
+      
+    def ReachableGroundFootprint(self, altitude, mu_deg, PowFailure):
+        alt = altitude * 0.3048 # cruising altitude in meters
+        self.rho = 1.225 # density of air in kg/m^3
+        self.g = 9.81 # acceleration due to gravity in m/s^2
+        g = self.g
+        speed = self.cruiseSpeed # cruising speed in mph
+        speed = speed * 0.44704 # converting mph to m/s 
+        W = self.MTOW * 9.81
+        T = (1 - (PowFailure/100)) * W
+        CD0 = 0.037
+        k = 0.037
+        
+        # Define Heading Change angles from 0 deg to 270 degrees, with a condition to stop when radial angle is 180 degrees
+        PSI = np.linspace(0, 3*math.pi/2, 100)
+        
+        """
+        For each PSI angle, find the x, y coordinates where it touches the ground
+        """
+        X_RHS = []
+        Y_RHS = []
+        Distance = []
+        ETA_final = []
+        
+        for i, HC in enumerate(PSI):
+            if HC <= math.pi/6:
+                mu_rad = self.degrees2radians(10)
+            elif HC <= math.pi/3:
+                mu_rad = self.degrees2radians(20)
+            elif HC <= math.pi/2:
+                mu_rad = self.degrees2radians(30)
+            elif HC <= 4*math.pi/6:
+                mu_rad = self.degrees2radians(40)
+            else:
+                mu_rad = self.degrees2radians(45)
+            # mu_rad = self.degrees2radians(mu_deg)
+            
+            r = self.FindTurnRadius(speed, mu_rad) # radius of the turn
+            Larc = self.FindTurnRadiusArcLength(r, HC)  # given change in heading, find arc length of the turn
+            L, D = self.EstimateLiftandDrag(speed, mu_rad)
+            VsV = self.EstimateSinkRate(D)
+            dH_turn = self.EstimateAltitudeLossDuringTurn(Larc, VsV, mu_rad)
+            DGlide = (alt - dH_turn) * (self.LDratio)
+
+            if DGlide >= 0:
+                x = r * math.cos(math.pi - HC) + r
+                y = r * math.sin(math.pi - HC)                    
+                x = x + DGlide * math.sin(HC)
+                y = y + DGlide * math.cos(HC)
+                ETA_estimated = math.atan(x/y)
+                if (x > 0 and y < 0) or (x < 0 and y < 0):
+                    ETA_estimated = math.pi + ETA_estimated
+                if ETA_estimated >= math.pi:
+                    break
+            elif DGlide < 0:
+                hc = np.linspace(0,PSI[i-1],100)
+                X_turn = []
+                Y_turn = []
+                TurnDistance = []
+                ETA = []
+                for j in hc:
+                    x = r * math.cos(math.pi - j) + r
+                    y = r * math.sin(math.pi - j)
+                    X_turn.append(x)
+                    Y_turn.append(y)
+                    TurnDistance.append(math.sqrt(x ** 2 + y ** 2))
+                    if x >= 0 and y >= 0:
+                        if y == 0:
+                            ETA.append(math.pi/2)
+                        else:
+                            ETA.append(math.atan(x/y))
+                    elif (x > 0 and y < 0) or (x < 0 and y < 0):
+                        ETA.append(math.pi + math.atan(x/y))
+                    elif (x < 0 and y > 0):
+                        ETA.append(2 * math.pi + math.atan(x/y))
+                # print("I")
+                break
+            
+            X_RHS.append(x)
+            Y_RHS.append(y)
+            Distance.append(math.sqrt(x ** 2 + y ** 2))
+            ETA_final.append(ETA_estimated)    
+        
+        try: # if footprint is not an ellipsoid
+            X_RHS, Y_RHS = self.Combine(X_turn, Y_turn, self.Reverse(X_RHS), self.Reverse(Y_RHS))
+            X_LHS, Y_LHS = self.Symmetry(X_RHS,Y_RHS)
+            X, Y = self.Combine(X_RHS, Y_RHS, X_LHS, Y_LHS)
+                
+            Distance_RHS, NullArray = self.Combine(TurnDistance, [1], self.Reverse(Distance), [1])
+            Distance_LHS = Distance_RHS
+            Distance, NullArray = self.Combine(Distance_RHS, [1], self.Reverse(Distance_LHS), [1])
+            
+            ETA_RHS, NullArray = self.Combine(ETA, [1], self.Reverse(ETA_final),[1])
+            ETA_LHS = np.multiply(-1,ETA_RHS)
+            ETA_LHS = self.Reverse(ETA_LHS)
+            ETA_final, NullArray = self.Combine(ETA_RHS, [1], ETA_LHS, [1])
+        except:
+            X_LHS, Y_LHS = self.Symmetry(X_RHS,Y_RHS)
+            X, Y = self.Combine(X_RHS, Y_RHS, X_LHS, Y_LHS)
+            
+            Distance_RHS = Distance
+            Distance_LHS = Distance_RHS
+            Distance, NullArray = self.Combine(Distance_RHS, [1], self.Reverse(Distance_LHS), [1])
+        
+            ETA_RHS = ETA_final
+            ETA_LHS = np.multiply(-1,ETA_RHS)
+            ETA_LHS = self.Reverse(ETA_LHS)
+            ETA_final, NullArray = self.Combine(ETA_RHS, [1], ETA_LHS, [1])
+        
+        # self.PlotReachabilityFootprint(X, Y)
+        # self.PolarPlotReachabilityFootprint(ETA_final, np.multiply(1/1000,Distance))
+        
+        # print(Distance)
+        return (X, Y, ETA_final, np.multiply(1/1000,Distance))
+        
+    def FindTurnRadius(self, speed, mu):
+        r = speed ** 2 / (self.g * math.tan(mu)) # radius of the turn
+        return r    
+    
+    def FindTurnRadiusArcLength(self, r, hc):
+        Larc = r * abs(hc)
+        return Larc
+    
+    def EstimateLiftandDrag(self, speed, mu):
+        CL = 2 * (self.MTOW * self.g) / (self.rho * self.S * speed ** 2)
+        CD = CL/self.LDratio # Find CD from L/D ratio
+        L = 0.5 * self.rho * speed ** 2 * self.S * CL * math.cos(mu)
+        D = 0.5 * self.rho * speed**2 * self.S * CD
+        return (L, D)
+    
+    def EstimateSinkRate(self, D):
+        return D/(self.MTOW*self.g)
+    
+    def EstimateAltitudeLossDuringTurn(self, Larc, VsV, mu):
+        dH_turn = Larc * VsV / math.cos(mu) # height loss
+        return dH_turn
+    
+    def Symmetry(self,X_RHS,Y_RHS):
+        X_LHS = np.multiply(-1,X_RHS)
+        Y_LHS = Y_RHS
+        
+        X_LHS = self.Reverse(X_LHS) #reversing using list slicing
+        Y_LHS = self.Reverse(Y_LHS) #reversing using list slicing
+        return (X_LHS, Y_LHS)
+    
+    def Reverse(self, x):
+        x = x[::-1]
+        return x
+    
+    def Combine(self, xr, yr, xl, yl):
+        x = list(itertools.chain(xr,xl))
+        y = list(itertools.chain(yr,yl))
+        return (x, y)
+    
+    def PlotReachabilityFootprint(self, X, Y):
+        fig, ax = plt.subplots(figsize=(8,5), dpi=300)
+        ax.plot(X, Y)
+        plt.show()
+    
+    def PolarPlotReachabilityFootprint(self, teta, r):
+        fig, ax = plt.subplots(subplot_kw=dict(polar=True), dpi=300)
+        ax.plot(teta, r)
+        ax.set_theta_direction(-1) # CW direction 
+        # ax.set_theta_zero_location('N')
+        plt.show() # comment this line when comparing all vehicles; uncomment when running for big code
+    
+    def radians2degrees(self,angle):
+        try:
+            angle = angle * 180 / math.pi
+        except:
+            for i in range(len(angle)):
+                angle[i] = angle[i] * 180 / math.pi
+        return angle
+    
+    def degrees2radians(self,angle):
+        angle = angle * math.pi / 180
+        return angle
+        # TurnRadius = (speed ** 2) * math.cos(gamma) / (g * math.tan(mu))
+        
+
+        
+# JobyS4 = Aircraft("Joby", 4, 200, 150, 13.8, 45, 2177, 254.4, S=10.7*1.7)
+# JobyS4.Characteristics()
+# JobyS4.ReachableGroundFootprint(100,35,0)
+
+# Lilium7 = Aircraft("Joby", 7, 186, 186, 16.3, 60, 1700, 187.8, S=10.7*1.7)
+# Lilium7.Characteristics()
+# Lilium7.ReachableGroundFootprint(1500,35,0)
+
+# Archer5 = Aircraft("Joby", 5, 175, 60, 11.3, 24, 3175, 160, S=10.7*1.7)
+# Archer5.Characteristics()
+# Archer5.ReachableGroundFootprint(1500,35,0)
+
+# Volocopter1 = Aircraft("Joby", 1, 56, 22, 2.5, 19, 900, 83.3, S=10.7*1.7)
+# Volocopter1.Characteristics()
+# Volocopter1.ReachableGroundFootprint(1500,35,0)
+
+# EHang1 = Aircraft("Joby", 1, 62, 22, 1.5, 21, 1322, 206.2, S=10.7*1.7)
+# EHang1.Characteristics()
+# EHang1.ReachableGroundFootprint(1500,35,0)
+# plt.show()
---
+++ Code_v2/LoadExistingUAMaerodromeInfrastructure.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Sai V. Mudumba
+Code Conceived: March, 2021
+Code Last Modified: May 15, 2021
+"""
+
+import pandas as pd # see pandas documentation
+import math
+
+class LoadExistingUAMaerodromeInfrastructure:
+    """
+    CLASS DEFINITION:
+        LoadExistingUAMAerodromeInfrastructure is a class that
+        - loads an excel spreadsheet (.xlsx) of aerodrome locations from a folder
+        - converts the lat, lon coordinates from degrees to mercator so that they can be plotted on a tilemap using Bokeh library
+        *** see the excel document to see how it is formatted ***
+        
+    INPUT(S):
+        Name of the metropolitan area (e.g., "Chicago" or "Dallas")
+    
+    ATTRIBUTE(S):
+        1. MetroName: name of the metropolitan area (e.g., "Chicago") 
+        2. FilePath: this is the location of the excel spreadsheet
+        3. lat_regional_merc: read latitude information in mercator coordinates
+        4. lon_regional_merc: read longitude information in mercator coordinates
+        5. lat_regional_deg: read latitude information in degree coordinates
+        6. lon_regional_deg: read latitude information in degree coordinates
+        7. (...) same as above for other types of aerodromes (i.e., major and heliports)    
+    
+    METHOD(S):
+        1. ReadExcelSheetInMercatorOnly - reads lat, lon coordinates (provided in degrees) in mercator coordinates, with units in meters
+        2. ReadExcelSheetInDegreesOnly - reads lat, lon coordinates as provided in the excel spreadsheet, without converting it to mercator coordinates
+        3. InMercator - this function creates two more columns in the pandas database for lat and lon in mercator coordinates
+        4. Degrees2Mercator - converts (lat, lon) from degrees to mercator coordinates
+        5. Mercator2Degrees - converts (lat, lon) from mercator to degrees
+    """
+    
+    def __init__(self, MetroName):
+        self.MetroName = MetroName
+        self.FilePath = "C:/Users/Sai Mudumba/Documents/MSAAE_Thesis_Code/Datasets/" + self.MetroName + "/" + self.MetroName + ".xlsx"
+        
+        self.lat_regional_merc, self.lon_regional_merc = self.ReadExcelSheetInMercatorOnly(self.FilePath, "Regional")
+        self.lat_major_merc, self.lon_major_merc = self.ReadExcelSheetInMercatorOnly(self.FilePath, "Major")
+        self.lat_heliports_merc, self.lon_heliports_merc = self.ReadExcelSheetInMercatorOnly(self.FilePath, "Heliports")
+        
+        self.lat_regional_deg, self.lon_regional_deg = self.ReadExcelSheetInDegreesOnly(self.FilePath, "Regional")
+        self.lat_major_deg, self.lon_major_deg = self.ReadExcelSheetInDegreesOnly(self.FilePath, "Major")
+        self.lat_heliports_deg, self.lon_heliports_deg = self.ReadExcelSheetInDegreesOnly(self.FilePath, "Heliports")
+
+    def ReadExcelSheetInMercatorOnly(self, FilePath, SheetName):
+        loadSheet = pd.read_excel(FilePath, sheet_name = SheetName)
+        lat_deg = loadSheet["LatD"]
+        lon_deg = loadSheet["LonD"]
+        lat_merc, lon_merc = self.InMercator(lat_deg, lon_deg)
+        return lat_merc, lon_merc
+    
+    def ReadExcelSheetInDegreesOnly(self, FilePath, SheetName):
+        loadSheet = pd.read_excel(FilePath, sheet_name = SheetName)
+        lat_deg = loadSheet["LatD"]
+        lon_deg = loadSheet["LonD"]
+        return lat_deg, lon_deg
+        
+    def InMercator(self, lat_deg, lon_deg):
+        d = {"LatD" : lat_deg, "LonD" : lon_deg} # a dictionary - keys: Lat, Lon - values: list of coordinates in degrees
+        all_p = pd.DataFrame(data = d) # creates a 2D data structure, with two columns named LatD, LonD
+        
+        lat_merc = []
+        lon_merc = []
+        for i in range(len(all_p)):
+            lat, lon = self.Degrees2Mercator((all_p["LatD"][i], all_p["LonD"][i]))
+            lat_merc.append(lat)
+            lon_merc.append(lon)
+        
+        return lat_merc, lon_merc
+    
+    def Degrees2Mercator(self, Coords):
+        """
+        Takes in a tuple as an input.
+        Converts to Mercator Coordinates from Latitude, Longitude in degrees
+        Input = e.g., (42, -88)
+        Output = e.g., (-9780000, -9745000)
+        """
+        Coordinates = Coords
+    
+        lat = Coordinates[0]
+        lon = Coordinates[1]
+    
+        r_major = 6378137.000
+        x = r_major * math.radians(lon)
+        scale = x/lon
+        y = 180.0/math.pi * math.log(math.tan(math.pi/4.0 + lat * (math.pi/180.0)/2.0)) * scale
+    
+        return (x, y)
+    
+    def Mercator2Degrees(self, x, y):
+        """
+        https://www.usna.edu/Users/oceano/pguth/md_help/html/mapb0iem.htm
+        """
+        R = 6378137.000
+        lat = math.pi/2 - 2 * math.atan(math.exp(-y/R))
+        lon = x/R
+        return (lat*180/math.pi, lon*180/math.pi)        
+        
+    def __call__(self):
+        print(self.lat_regional_merc, self.lon_regional_merc, self.lat_major_merc, self.lon_major_merc, self.lat_heliports_merc, self.lon_heliports_merc)
+        return self.lat_regional_merc, self.lon_regional_merc, self.lat_major_merc, self.lon_major_merc, self.lat_heliports_merc, self.lon_heliports_merc
+    
