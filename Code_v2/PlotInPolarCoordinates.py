0f719534078fa66f016f1bb4ad9e5b9abe5c6e81
Sai Mudumba <56524843+smudumba2019@users.noreply.github.com>
2021-06-26 15:40:22 -0500

    Add files via upload

---
+++ Code_v2/PlotInPolarCoordinates.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 20:40:42 2021
+
+@author: Sai Mudumba
+"""
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def PlotInPolarCoordinates(distance, direction, arrowprops=None):
+    """
+    Reference: https://ocefpaf.github.io/python4oceanographers/blog/2015/02/09/compass/
+    Compass draws a graph that displays the vectors with
+    components `u` and `v` as arrows from the origin.
+
+    """
+    angles = np.multiply(direction, math.pi/180)
+    radii = distance
+
+    fig, ax = plt.subplots(subplot_kw=dict(polar=True), dpi = 300, figsize=(8,5))
+    kw = dict(arrowstyle="->", color='k')
+    if arrowprops:
+        kw.update(arrowprops)
+    [ax.annotate("", xy=(angle, radius), xytext=(0, 0),
+                 arrowprops=kw) for
+     angle, radius in zip(angles, radii)]
+    ax.set_ylim(0, 10)
+    return (fig, ax)
---
+++ Code_v2/run_script.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Sai V. Mudumba
+Code Conceived: March, 2021
+Code Last Modified: May 15, 2021
+"""
+
+# from main import *
+from main_ForTestingPurposesOnly import *
+from PlotInPolarCoordinates import *
+from generate_video import *
+from FlightProfile import *
+from Aircraft import *
+from TripMapper import *
+import os
+
+import math
+import matplotlib.pyplot as plt
+
+
+def RunScript(Option, SaveMap = False, GenerateMapVideo = False, GeneratePolarPlotVideo = False):
+    if Option == 1: # IF YOU WANT TO PLOT FOR A SPECIFIC CRUISING ALTITUDE, RUN THIS CODE
+        PlotMultipleTrips(3280, True) # input is the cruising altitude in ft
+    elif Option == 2: # IF YOU WANT TO PLOT CLAP VS ALTITUDE, RUN THIS CODE
+        altitude, CLAP = PlotCLAP()
+    else:
+        print("Pick the correct option")
+    
+    if GenerateMapVideo == True:
+        """
+        GENERATE VIDEO FROM THE ABOVE TRIP SEQUENCE
+        """
+        # Calling the generate_video function
+        image_folder = "C:\\Users\\Sai Mudumba\\Documents\\MSAAE_Thesis_Code\\Images\\TripAnimation\\" # make sure to use your folder
+        video_name = 'DuPage2JHSHH.avi'
+        os.chdir("C:\\Users\\Sai Mudumba\\Documents\\MSAAE_Thesis_Code\\Images\\TripAnimation")
+        generate_video(image_folder, video_name, 10)
+        
+    if GeneratePolarPlotVideo == True:
+        """
+        GENERATE VIDEO FROM THE ABOVE SEQUENCE OF POLAR PLOTS
+        """
+        # Calling the generate_video function
+        image_folder = "C:\\Users\\Sai Mudumba\\Documents\\MSAAE_Thesis_Code\\Images\\Compass\\" # make sure to use your folder
+        video_name = 'DuPage2JHSHH.avi'
+        os.chdir("C:\\Users\\Sai Mudumba\\Documents\\MSAAE_Thesis_Code\\Images\\Compass")
+        generate_video(image_folder, video_name, 10)
+    try:
+        return (altitude, CLAP)
+    except:
+        return None
+RunScript(2, GenerateMapVideo = False, GeneratePolarPlotVideo = False)
+
+
+# altitudeD, CLAPD = RunScript(2, GenerateMapVideo = False, GeneratePolarPlotVideo = False)
+# altitudeND, CLAPND = RunScript(2, GenerateMapVideo = False, GeneratePolarPlotVideo = False)
+
+# #  PLOTTING MULTIPLE CLAP VS ALTITUDES FOR DIRECT AND NON-DIRECT ROUTES 
+# #  UNCOMMENT ONLY WHEN IT IS NEEDED
+# fig, ax = plt.subplots(figsize=(8,5))
+# ax.plot(altitudeD, CLAPD,linewidth='3',color='blue',label='Direct Route: 30 miles long')
+# ax.plot(altitudeND, CLAPND, linewidth='3', color='red',label='Non-direct Route: 3.3% longer')
+# ax.set_xlabel('Cruise Altitude Floor (ft.)')
+# ax.set_ylabel('Contingency Landing Assurance Percentage (%)')
+# ax.set_title('Contingency Landing Assurance Pecentage vs. Cruise Altitude Floor')
+# ax.set_yticks([0,10,20,30,40,50,60,70,80,90,100])
+# ax.set_xticks(np.arange(1500,7500,500))
+# ax.minorticks_on()
+# ax.grid(which='major', linestyle='-', linewidth='0.25', color='black')
+# ax.grid(which='minor', linestyle=':', linewidth='0.25', color='black')
+# ax.legend(loc='lower right')
+# plt.savefig('C:/Users/Sai Mudumba/Documents/MSAAE_Thesis_Code/Images/AllCLAPvsAltRevised.png', dpi=900)
+# plt.show()
