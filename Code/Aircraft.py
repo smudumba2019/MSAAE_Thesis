8c0c01dd39aac68ae3be00834c22975bf5e000ff
Sai Mudumba <56524843+smudumba2019@users.noreply.github.com>
2021-10-23 11:03:18 -0400

    Add files via upload

---
+++ Code/Aircraft.py
@@ -0,0 +1,293 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 15:33:44 2021
+
+@author: Sai Mudumba
+"""
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+from shapely.geometry import Polygon
+
+class Aircraft:
+    def __init__(self, ACname, PAX, Speed_Cr, Range, LD_Ratio, Max_Flt_Time, MTOW, KWh_Batt, S):
+        # Later on, this will be replaced by loading an excel sheet that automatically loads all the characteristics
+        # This is written here only for ease of running the code
+        self.acName = ACname
+        self.pax = PAX
+        self.cruiseSpeed = Speed_Cr
+        self.acRange = Range
+        self.LDratio = LD_Ratio
+        self.maxFltTime = Max_Flt_Time
+        self.MTOW = MTOW
+        self.kWhBatt = KWh_Batt
+        self.S = S
+        self.DGlideStraight = 0
+      
+    def Characteristics(self):
+        print(f"These are the aircraft characteristics for {self.acName}: ")
+        print(f"Aircraft Name: {self.acName}")
+        print(f"Passenger Capacity: {self.pax}")
+        print(f"Maximum Cruise Speed (mph): {self.cruiseSpeed}")
+        print(f"Range (mi): {self.acRange}")
+        print(f"Lift to Drag Ratio (Cruise): {self.LDratio}")
+        print(f"Endurance (minutes): {self.maxFltTime}")
+        print(f"Maximum Takeoff Weight (kg): {self.MTOW}")
+        print(f"Battery Capacity (kWh): {self.kWhBatt}")
+        print(f"Wing Surface Area (m^2): {self.S}")
+      
+    def ReachableGroundFootprint(self, altitude, speed, mu_deg, PowFailure):
+        alt = altitude * 0.3048 # cruising altitude in meters
+        self.rho = 1.225 # density of air in kg/m^3
+        self.g = 9.81 # acceleration due to gravity in m/s^2
+        g = self.g
+        speed = speed # in m/s # self.cruiseSpeed # cruising speed in mph
+        # speed = speed * 0.44704 # converting mph to m/s 
+        W = self.MTOW * 9.81
+        T = (1 - (PowFailure/100)) * W
+        CD0 = 0.037
+        k = 0.037
+        
+        # Define Heading Change angles from 0 deg to 270 degrees, with a condition to stop when radial angle is 180 degrees
+        PSI = np.linspace(0, 3*math.pi/2, 100)
+        
+        """
+        For each PSI angle, find the x, y coordinates where it touches the ground
+        """
+        X_RHS = []
+        Y_RHS = []
+        Distance = []
+        ETA_final = []
+        
+        for i, HC in enumerate(PSI):
+            if HC <= math.pi/6:
+                mu_rad = self.degrees2radians(10)
+            elif HC <= math.pi/3:
+                mu_rad = self.degrees2radians(20)
+            elif HC <= math.pi/2:
+                mu_rad = self.degrees2radians(30)
+            elif HC <= 4*math.pi/6:
+                mu_rad = self.degrees2radians(40)
+            else:
+                mu_rad = self.degrees2radians(45)
+            # mu_rad = self.degrees2radians(mu_deg)
+            
+            r = self.FindTurnRadius(speed, mu_rad) # radius of the turn
+            Larc = self.FindTurnRadiusArcLength(r, HC)  # given change in heading, find arc length of the turn
+            L, D = self.EstimateLiftandDrag(speed, mu_rad)
+            VsV = self.EstimateSinkRate(D)
+            dH_turn = self.EstimateAltitudeLossDuringTurn(Larc, VsV, mu_rad)
+            DGlide = (alt - dH_turn) * (self.LDratio)
+
+            if DGlide >= 0:
+                x = r * math.cos(math.pi - HC) + r
+                y = r * math.sin(math.pi - HC)                    
+                x = x + DGlide * math.sin(HC)
+                y = y + DGlide * math.cos(HC)
+                ETA_estimated = math.atan(x/y)
+                if (x > 0 and y < 0) or (x < 0 and y < 0):
+                    ETA_estimated = math.pi + ETA_estimated
+                if ETA_estimated >= math.pi:
+                    break
+            elif DGlide < 0:
+                hc = np.linspace(0,PSI[i-1],100)
+                X_turn = []
+                Y_turn = []
+                TurnDistance = []
+                ETA = []
+                for j in hc:
+                    x = r * math.cos(math.pi - j) + r
+                    y = r * math.sin(math.pi - j)
+                    X_turn.append(x)
+                    Y_turn.append(y)
+                    TurnDistance.append(math.sqrt(x ** 2 + y ** 2))
+                    if x >= 0 and y >= 0:
+                        if y == 0:
+                            ETA.append(math.pi/2)
+                        else:
+                            ETA.append(math.atan(x/y))
+                    elif (x > 0 and y < 0) or (x < 0 and y < 0):
+                        ETA.append(math.pi + math.atan(x/y))
+                    elif (x < 0 and y > 0):
+                        ETA.append(2 * math.pi + math.atan(x/y))
+                # print("I")
+                break
+            
+            X_RHS.append(x)
+            Y_RHS.append(y)
+            Distance.append(math.sqrt(x ** 2 + y ** 2))
+            ETA_final.append(ETA_estimated)    
+        
+        try: # if footprint is not an ellipsoid
+            X_RHS, Y_RHS = self.Combine(X_turn, Y_turn, self.Reverse(X_RHS), self.Reverse(Y_RHS))
+            X_LHS, Y_LHS = self.Symmetry(X_RHS,Y_RHS)
+            X, Y = self.Combine(X_RHS, Y_RHS, X_LHS, Y_LHS)
+                
+            Distance_RHS, NullArray = self.Combine(TurnDistance, [1], self.Reverse(Distance), [1])
+            Distance_LHS = Distance_RHS
+            Distance, NullArray = self.Combine(Distance_RHS, [1], self.Reverse(Distance_LHS), [1])
+            
+            ETA_RHS, NullArray = self.Combine(ETA, [1], self.Reverse(ETA_final),[1])
+            ETA_LHS = np.multiply(-1,ETA_RHS)
+            ETA_LHS = self.Reverse(ETA_LHS)
+            ETA_final, NullArray = self.Combine(ETA_RHS, [1], ETA_LHS, [1])
+        except:
+            X_LHS, Y_LHS = self.Symmetry(X_RHS,Y_RHS)
+            X, Y = self.Combine(X_RHS, Y_RHS, X_LHS, Y_LHS)
+            
+            Distance_RHS = Distance
+            Distance_LHS = Distance_RHS
+            Distance, NullArray = self.Combine(Distance_RHS, [1], self.Reverse(Distance_LHS), [1])
+        
+            ETA_RHS = ETA_final
+            ETA_LHS = np.multiply(-1,ETA_RHS)
+            ETA_LHS = self.Reverse(ETA_LHS)
+            ETA_final, NullArray = self.Combine(ETA_RHS, [1], ETA_LHS, [1])
+        
+        # self.PlotReachabilityFootprint(X, Y)
+        # self.PolarPlotReachabilityFootprint(ETA_final, np.multiply(1/1000,Distance))
+        
+        # print(Distance)
+        return (X, Y, ETA_final, np.multiply(1/1000,Distance))
+        
+    def FindTurnRadius(self, speed, mu):
+        r = speed ** 2 / (self.g * math.tan(mu)) # radius of the turn
+        return r    
+    
+    def FindTurnRadiusArcLength(self, r, hc):
+        Larc = r * abs(hc)
+        return Larc
+    
+    def EstimateLiftandDrag(self, speed, mu):
+        CL = 2 * (self.MTOW * self.g) / (self.rho * self.S * speed ** 2)
+        CD = CL/self.LDratio # Find CD from L/D ratio
+        L = 0.5 * self.rho * speed ** 2 * self.S * CL * math.cos(mu)
+        D = 0.5 * self.rho * speed**2 * self.S * CD
+        # print(L/D)
+        return (L, D)
+    
+    def EstimateSinkRate(self, D):
+        return math.sin(math.atan(1/self.LDratio))#D/(self.MTOW*self.g)
+    
+    def EstimateAltitudeLossDuringTurn(self, Larc, VsV, mu):
+        dH_turn = Larc * VsV / math.cos(mu) # height loss
+        return dH_turn
+    
+    def Symmetry(self,X_RHS,Y_RHS):
+        X_LHS = np.multiply(-1,X_RHS)
+        Y_LHS = Y_RHS
+        
+        X_LHS = self.Reverse(X_LHS) #reversing using list slicing
+        Y_LHS = self.Reverse(Y_LHS) #reversing using list slicing
+        return (X_LHS, Y_LHS)
+    
+    def Reverse(self, x):
+        x = x[::-1]
+        return x
+    
+    def Combine(self, xr, yr, xl, yl):
+        x = list(itertools.chain(xr,xl))
+        y = list(itertools.chain(yr,yl))
+        return (x, y)
+    
+    def PlotReachabilityFootprint(self, X, Y):
+        fig, ax = plt.subplots(figsize=(8,5), dpi=300)
+        ax.plot(X, Y)
+        plt.show()
+    
+    def PolarPlotReachabilityFootprint(self, teta, r):
+        fig, ax = plt.subplots(subplot_kw=dict(polar=True), dpi=300)
+        ax.plot(teta, r)
+        ax.set_theta_direction(-1) # CW direction 
+        # ax.set_theta_zero_location('N')
+        plt.show() # comment this line when comparing all vehicles; uncomment when running for big code
+    
+    def radians2degrees(self,angle):
+        try:
+            angle = angle * 180 / math.pi
+        except:
+            for i in range(len(angle)):
+                angle[i] = angle[i] * 180 / math.pi
+        return angle
+    
+    def degrees2radians(self,angle):
+        angle = angle * math.pi / 180
+        return angle
+        # TurnRadius = (speed ** 2) * math.cos(gamma) / (g * math.tan(mu))
+    
+    def EstimateGlideRateOfDescent(self):
+        speed = self.cruiseSpeed # cruising speed in mph
+        speed = speed * 0.44704 # converting mph to m/s
+        CL = 2 * (self.MTOW * self.g) / (self.rho * self.S * speed ** 2)
+        ROD_glide = math.sqrt((self.MTOW * self.g) / (0.5 * self.rho * self.S * CL)) * (1/self.LDratio)
+        return ROD_glide
+    
+    def EstimateAltitudeDropInGlide(self,altitude, startle_delay):
+        altitude = 0.3048 * altitude # feet to meters
+        ROD_glide = self.EstimateGlideRateOfDescent()
+        final_altitude = altitude - (startle_delay * ROD_glide)
+        final_altitude = 3.28084 * final_altitude # meters to feet
+        return (final_altitude)
+
+        
+# JobyS4 = Aircraft("Joby", 4, 200, 150, 13.8, 45, 2177, 254.4, S=10.7*1.7)
+# JobyS4.Characteristics()
+# """
+# Determine the Reachable Footprint and Estimate its Footprint Area
+# """
+# X, Y, ETA, DistanceRadial = JobyS4.ReachableGroundFootprint(1500,35,0)
+# RGF_Area_Ref = Polygon(zip(X, Y)) # Assuming the OP's x,y coordinates
+# RGF_Area_Ref = RGF_Area_Ref.area/(1000**2) # in km^2
+# print(f"The Reachable Ground Footprint (RGF) Area for 1500 ft cruising altitude is: {RGF_Area_Ref} km^2")
+# plt.plot(X,Y)
+# plt.show()
+# """
+# Estimate the altitude loss in the case of a delayed response to emergency power failure case.
+# """
+# AltitudeLoss = []
+# Loss_RGF_Area = []
+# PercentLoss_RGF_Area = []
+# timeDelay = range(0,10,1)
+
+# for t in timeDelay:
+#     AltitudeDrop = JobyS4.EstimateAltitudeDropInGlide(1500,t)
+#     AltitudeLoss.append(AltitudeDrop - 1500)
+    
+#     X, Y, ETA, DistanceRadial = JobyS4.ReachableGroundFootprint(AltitudeDrop,35,0)
+#     RGF_Area = Polygon(zip(X, Y)) # Assuming the OP's x,y coordinates
+#     RGF_Area = RGF_Area.area/(1000**2)
+#     Loss_RGF_Area.append(RGF_Area)
+    
+#     Percent_Loss_Of_RGB_Area = -100 * (RGF_Area - RGF_Area_Ref) / RGF_Area_Ref
+#     PercentLoss_RGF_Area.append(Percent_Loss_Of_RGB_Area)    
+    
+#     # plt.plot(X,Y)
+#     # plt.show()
+    
+# plt.plot(timeDelay, PercentLoss_RGF_Area)
+# plt.show()
+
+# plt.plot(timeDelay, Loss_RGF_Area)
+# plt.show()
+
+# print(AltitudeLoss)
+# print(PercentLoss_RGF_Area)
+
+# Lilium7 = Aircraft("Joby", 7, 186, 186, 16.3, 60, 1700, 187.8, S=10.7*1.7)
+# Lilium7.Characteristics()
+# Lilium7.ReachableGroundFootprint(1500,35,0)
+
+# Archer5 = Aircraft("Joby", 5, 175, 60, 11.3, 24, 3175, 160, S=10.7*1.7)
+# Archer5.Characteristics()
+# Archer5.ReachableGroundFootprint(1500,35,0)
+
+# Volocopter1 = Aircraft("Joby", 1, 56, 22, 2.5, 19, 900, 83.3, S=10.7*1.7)
+# Volocopter1.Characteristics()
+# Volocopter1.ReachableGroundFootprint(1500,35,0)
+
+# EHang1 = Aircraft("Joby", 1, 62, 22, 1.5, 21, 1322, 206.2, S=10.7*1.7)
+# EHang1.Characteristics()
+# EHang1.ReachableGroundFootprint(1500,35,0)
+# plt.show()
---
+++ Code/run_script.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Sai V. Mudumba
+Code Conceived: March, 2021
+Code Last Modified: May 15, 2021
+"""
+
+# from main import *
+from main_ForTestingPurposesOnly import *
+from PlotInPolarCoordinates import *
+from generate_video import *
+from FlightProfile import *
+from Aircraft import *
+from TripMapper import *
+import os
+
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def PlotAllCLAP(altitude_Joby, CLAP_Joby, altitude_Lilium, CLAP_Lilium, altitude_Archer, CLAP_Archer, altitude_Volocopter, CLAP_Volocopter, altitude_Ehang, CLAP_Ehang):
+    fig, ax = plt.subplots(figsize=(8,5))
+    ax.plot(altitude_Joby, CLAP_Joby, linewidth='3',color='blue',label='Joby')
+    ax.plot(altitude_Lilium, CLAP_Lilium, linewidth='3', color='red',label='Lilium')
+    ax.plot(altitude_Archer, CLAP_Archer, linewidth='3', color='green',label='Archer')
+    ax.plot(altitude_Volocopter, CLAP_Volocopter, linewidth='3', color='magenta',label='Volocopter')
+    ax.plot(altitude_Ehang, CLAP_Ehang, linewidth='3', color='black',label='EHang')
+    
+    ax.set_xlabel('Cruise Altitude (ft.)')
+    ax.set_ylabel('Contingency Landing Assurance Percentage (%)')
+    ax.set_title('Contingency Landing Assurance Pecentage vs. Cruise Altitude')
+    ax.set_yticks([0,10,20,30,40,50,60,70,80,90,100])
+    ax.set_xticks(np.arange(1500,7500,500))
+    ax.minorticks_on()
+    ax.grid(which='major', linestyle='-', linewidth='0.25', color='black')
+    ax.grid(which='minor', linestyle=':', linewidth='0.25', color='black')
+    ax.legend(loc='center right')
+    plt.savefig('C:/Users/saimu/Documents/AAE_MS_Thesis_Documentation_v2/Python_Code/Results/AllCLAPvsAltRevised.png', dpi=600)
+    plt.show()
+
+def FindCruiseAltitudeFloor(altitude, CLAP):
+    for i in range(0, len(altitude)):
+        if CLAP[i] >= 95:
+            CAF = altitude[i]
+            break
+    return CAF
+    
+def RunScript(TripInfo, CAF, Option, SaveMap = False, GenerateMapVideo = False, GeneratePolarPlotVideo = False, PlotGraphs = False):
+    """
+    Parameters
+    ----------
+    TripInfo
+        DepType = "Heliport"
+        DepartureID = 7
+        ArrType = "Regional"
+        ArrivalID = 8
+    
+    Option : integer
+        This input tells the function what analysis to run. 
+        Option 1 means plot multiple trips, their respective distance and polar plots 
+        Option 2 means plot CLAP versus various cruising altitudes
+        
+    SaveMap : boolean, optional
+        Do you want to save the map? The default is False.
+    
+    GenerateMapVideo : boolean, optional
+        Do you want to generate map video from the map images you saved? The default is False.
+    
+    GeneratePolarPlotVideo : boolean, optional
+        Do you want to generate polar plots from the polar plots you saved? The default is False.
+
+    Returns
+    -------
+    altitude : array
+        an array of cruise altitudes in feet
+    CLAP : array
+        an array of Contingency Landing Assurance Percentages (CLAP)
+    """
+    
+    Joby = Aircraft("Joby", 4, 200, 150, 13.8, 45, 2177, 200, S=10.7*1.7)
+    Lilium7 = Aircraft("Lilium", 7, 186, 186, 16.3, 60, 1700, 187.8, S=10.7*1.7)
+    Archer5 = Aircraft("Archer", 5, 175, 60, 11.3, 24, 3175, 160, S=10.7*1.7)
+    Volocopter1 = Aircraft("Volocopter", 1, 70.6, 22, 2.5, 19, 900, 83.3, S=1.7*1.7)
+    EHang1 = Aircraft("Ehang", 1, 62, 22, 1.5, 21, 1322, 206.2, S=10.7*1.7)
+    AAE451 = Aircraft("AAE451", 1, 50, 8, 11.5, 10, 5.6, 0.048, 0.5)
+    
+    if Option == 1: # IF YOU WANT TO PLOT FOR A SPECIFIC CRUISING ALTITUDE, RUN THIS CODE
+        CLAP_Joby_atCAF, FltTime_Joby, EneConsumed_Joby = PlotMultipleTrips(TripInfo, Joby, CAF[0], False, False, PlotGraphs) 
+        CLAP_Lilium_atCAF, FltTime_Lilium, EneConsumed_Lilium = PlotMultipleTrips(TripInfo, Lilium7, CAF[1], False, False, PlotGraphs) 
+        CLAP_Archer_atCAF, FltTime_Archer, EneConsumed_Archer = PlotMultipleTrips(TripInfo, Archer5, CAF[2], False, False, PlotGraphs) 
+        
+        # 1st input is TripInfo
+        # 2nd input is the cruising altitude in ft
+        # 3rd input is a boolean, to show the map once the analysis is done running
+        # 4th input is a boolean, to save the map once the analysis is done running
+        
+    elif Option == 2: # IF YOU WANT TO PLOT CLAP VS ALTITUDE, RUN THIS CODE
+        altitude_Joby, CLAP_Joby = PlotCLAP(TripInfo, Joby)
+        CAF_Joby = FindCruiseAltitudeFloor(altitude_Joby, CLAP_Joby)
+        
+        altitude_Lilium, CLAP_Lilium = PlotCLAP(TripInfo, Lilium7)
+        CAF_Lilium = FindCruiseAltitudeFloor(altitude_Lilium, CLAP_Lilium)
+        
+        altitude_Archer, CLAP_Archer = PlotCLAP(TripInfo, Archer5)
+        CAF_Archer = FindCruiseAltitudeFloor(altitude_Archer, CLAP_Archer)
+        
+        altitude_Volocopter, CLAP_Volocopter = PlotCLAP(TripInfo, Volocopter1)
+        altitude_Ehang, CLAP_Ehang = PlotCLAP(TripInfo, EHang1)
+        # PlotAllCLAP(altitude_Joby, CLAP_Joby, altitude_Lilium, CLAP_Lilium, altitude_Archer, CLAP_Archer, altitude_Volocopter, CLAP_Volocopter, altitude_Ehang, CLAP_Ehang)
+        PlotAllCLAP(altitude_Joby, CLAP_Joby, altitude_Lilium, CLAP_Lilium, altitude_Archer, CLAP_Archer, altitude_Volocopter, CLAP_Volocopter, altitude_Ehang, CLAP_Ehang)
+        # print(CAF_Joby, CAF_Lilium, CAF_Archer)
+        
+    else:
+        print("Pick the correct option")
+    
+    if GenerateMapVideo == True:
+        """
+        GENERATE VIDEO FROM THE ABOVE TRIP SEQUENCE
+        """
+        # Calling the generate_video function
+        image_folder = "C:\\Users\\Sai Mudumba\\Documents\\MSAAE_Thesis_Code\\Images\\TripAnimation\\" # make sure to use your folder
+        video_name = 'DuPage2JHSHH.avi'
+        os.chdir("C:\\Users\\Sai Mudumba\\Documents\\MSAAE_Thesis_Code\\Images\\TripAnimation")
+        generate_video(image_folder, video_name, 30)
+        
+    if GeneratePolarPlotVideo == True:
+        """
+        GENERATE VIDEO FROM THE ABOVE SEQUENCE OF POLAR PLOTS
+        """
+        # Calling the generate_video function
+        image_folder = "C:\\Users\\Sai Mudumba\\Documents\\MSAAE_Thesis_Code\\Images\\Compass\\" # make sure to use your folder
+        video_name = 'DuPage2JHSHH.avi'
+        os.chdir("C:\\Users\\Sai Mudumba\\Documents\\MSAAE_Thesis_Code\\Images\\Compass")
+        generate_video(image_folder, video_name, 30)
+    
+    try:
+        return (CAF_Joby, CAF_Lilium, CAF_Archer)
+    except:
+        return ((CLAP_Joby_atCAF, FltTime_Joby, EneConsumed_Joby),(CLAP_Lilium_atCAF, FltTime_Lilium, EneConsumed_Lilium),(CLAP_Archer_atCAF, FltTime_Archer, EneConsumed_Archer))
+
+
+
+
+"""
+RUN THE CODE FOR FINDING THE CRUISE ALTITUDE FLOOR OF EACH VEHICLE BELOW FOR MULTIPLE TRIPS IN A NETWORK
+"""
+
+DepType = "Regional"
+ArrType = "Heliport"
+DEP = [7] #[7, 9, 13, 9]
+ARR = [8] #[8, 6, 7, 11]
+
+# RunScript((DepType, DEP[0], ArrType, ARR[0]), CAF, 1, GenerateMapVideo = False, GeneratePolarPlotVideo = False, PlotGraphs = True)
+
+AerodromeKeys = [] # Store the from and to trip information as a string
+TripsCAF = [] # Store a list of Cruise Altitude Floor values with each column representing an eVTOL vehicle and each row being the trip
+TripsFlightTime = []
+TripsEnergyConsumed = []
+for dep in DEP: # example: DEP = [7, 9, 13, 9]
+    for arr in ARR: # example: ARR = [8, 6, 7, 11]
+        TripInfo = (DepType, dep, ArrType, arr) # a tuple that contains Departure Type, Departure ID, Arrival Type, Arrival ID
+        FromTo = DepType + " ID: " + str(dep) + " to " + ArrType + " ID: " + str(arr) # a string that summarizes trip info
+        
+        """The line below runs the script and gets the Cruise Altitude Floor Req for each vehicle, for each trip in a network"""
+        CAF_Joby, CAF_Lilium, CAF_Archer = RunScript(TripInfo, 0, 2, GenerateMapVideo = False, GeneratePolarPlotVideo = False, PlotGraphs = False)
+        Joby_Data, Lilium_Data, Archer_Data = RunScript(TripInfo, (CAF_Joby, CAF_Lilium, CAF_Archer), 1, GenerateMapVideo = False, GeneratePolarPlotVideo = False, PlotGraphs = False)
+
+        # Append to a List
+        TripsCAF.append([CAF_Joby, CAF_Lilium, CAF_Archer])
+        TripsFlightTime.append([Joby_Data[1], Lilium_Data[1], Archer_Data[1]])
+        TripsEnergyConsumed.append([Joby_Data[2], Lilium_Data[2], Archer_Data[2]])
+        AerodromeKeys.append([FromTo])
+
+# RunScript((DepType, DEP[0], ArrType, ARR[0]), CAF, 1, GenerateMapVideo = False, GeneratePolarPlotVideo = False, PlotGraphs = False)
+
+"""    
+1. CONVERT CRUISE ALTITUDE FLOORS TO A NUMPY ARRAY
+2. CREATE A PANDAS DATAFRAME
+3. EXPORT THE PANDAS DATAFRAME TO EXCEL
+
+Ref: 
+    https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
+    https://datatofish.com/export-dataframe-to-excel/
+
+"""
+TripsCAF_numpy = np.array(TripsCAF)
+df = pd.DataFrame(TripsCAF_numpy, columns=['Joby','Lilium','Archer'])
+df.to_excel (r'C:/Users/saimu/Documents/AAE_MS_Thesis_Documentation_v2/TripsCruiseAltitudeFloor_Dataframe.xlsx', index = False, header=True)
+
+TripsFlightTime_numpy = np.array(TripsFlightTime)
+df1 = pd.DataFrame(TripsFlightTime_numpy, columns=['Joby','Lilium','Archer'])
+df1.to_excel (r'C:/Users/saimu/Documents/AAE_MS_Thesis_Documentation_v2/TripsFlightTimes_Dataframe.xlsx', index = False, header=True)
+
+TripsEnergyConsumed_numpy = np.array(TripsEnergyConsumed)
+df2 = pd.DataFrame(TripsEnergyConsumed_numpy, columns=['Joby','Lilium','Archer'])
+df2.to_excel (r'C:/Users/saimu/Documents/AAE_MS_Thesis_Documentation_v2/TripsEnergyConsumed_Dataframe.xlsx', index = False, header=True)
+
+Trips_numpy = np.array(AerodromeKeys)
+df3 = pd.DataFrame(Trips_numpy, columns=['TripsInfo'])
+df3.to_excel (r'C:/Users/saimu/Documents/AAE_MS_Thesis_Documentation_v2/Trips_Dataframe.xlsx', index = False, header=True)
+
+# altitudeD, CLAPD = RunScript(2, GenerateMapVideo = False, GeneratePolarPlotVideo = False)
+# altitudeND, CLAPND = RunScript(2, GenerateMapVideo = False, GeneratePolarPlotVideo = False)
+
+# #  PLOTTING MULTIPLE CLAP VS ALTITUDES FOR DIRECT AND NON-DIRECT ROUTES 
+# #  UNCOMMENT ONLY WHEN IT IS NEEDED
+# fig, ax = plt.subplots(figsize=(8,5))
+# ax.plot(altitudeD, CLAPD,linewidth='3',color='blue',label='Direct Route: 30 miles long')
+# ax.plot(altitudeND, CLAPND, linewidth='3', color='red',label='Non-direct Route: 3.3% longer')
+# ax.set_xlabel('Cruise Altitude Floor (ft.)')
+# ax.set_ylabel('Contingency Landing Assurance Percentage (%)')
+# ax.set_title('Contingency Landing Assurance Pecentage vs. Cruise Altitude Floor')
+# ax.set_yticks([0,10,20,30,40,50,60,70,80,90,100])
+# ax.set_xticks(np.arange(1500,7500,500))
+# ax.minorticks_on()
+# ax.grid(which='major', linestyle='-', linewidth='0.25', color='black')
+# ax.grid(which='minor', linestyle=':', linewidth='0.25', color='black')
+# ax.legend(loc='lower right')
+# plt.savefig('C:/Users/Sai Mudumba/Documents/MSAAE_Thesis_Code/Images/AllCLAPvsAltRevised.png', dpi=900)
+# plt.show()
+
+#  PLOTTING MULTIPLE CLAP VS ALTITUDES FOR DIRECT AND NON-DIRECT ROUTES 
+#  UNCOMMENT ONLY WHEN IT IS NEEDED
+
